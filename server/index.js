216c1128b003f03256a28da38ebedf77e92ccef9
Helen Soh <helenjsoh@gmail.com>
2018-12-20 16:02:13 -0500

    Created server file

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const axios = require('axios');
+
+const bodyParser = require('body-parser');
+
+let app = express();
+app.use(express.static(__dirname + '/../client/dist'))
+
+
+
