a1c3bb0a40472f96a49e9fe1a594d433d8bbd213
Helen Soh <helenjsoh@gmail.com>
2018-12-27 15:30:13 -0500

    Set up CSS Grid for images, progress made with resizing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -12,16 +12,19 @@ class App extends React.Component {
         };
         // Bind methods here
     }
-    // Write methods here
+    // Write methods here   
     // Retrieve photos
 
+
+    
     render () {
         return (
-            <div>
-            <div>Hi</div>   
-            <Images/>
-            <LeftArrow/>
-            <RightArrow/>
+            <div className="wrapper">
+            <img className="a"src="https://a0.muscache.com/im/pictures/8a542c1f-b897-4127-8426-6c8e1a4903b7.jpg?aki_policy=x_large"></img> 
+            <img className="b"src="https://a0.muscache.com/im/pictures/14d7ab26-5373-4039-9c42-aa0d0874ad6b.jpg?aki_policy=x_large"></img> 
+            <img className="c"src="https://a0.muscache.com/im/pictures/14d7ab26-5373-4039-9c42-aa0d0874ad6b.jpg?aki_policy=x_large"></img> 
+            <img className="d"src="https://a0.muscache.com/im/pictures/14d7ab26-5373-4039-9c42-aa0d0874ad6b.jpg?aki_policy=x_large"></img> 
+            <img className="e"src="https://a0.muscache.com/im/pictures/14d7ab26-5373-4039-9c42-aa0d0874ad6b.jpg?aki_policy=x_large"></img> 
             </div>
         )
     }
